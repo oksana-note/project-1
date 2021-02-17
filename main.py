7d52b247c40a5819cd661a42ee7c1c7e5c86aaed
oksana-note <78647585+oksana-note@users.noreply.github.com>
2021-02-17 20:52:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+my_var = 26 , 46, 78,96.
+
+print(type(my_var))
+
+user_data = int (input("Какие числа Вам нужны?"))
+
+print(user_data,type(user_data))
+
+
+print(200 - user_data)
+
+user_message = int(input("Сколько будет 600 секунд в минутах и в часах?"))
+
+print(user_message)
+
+a = int(10)
+
+user_message = " минут {} ". format(a)
+
+print(user_message)
+
+user_message = (int(input("Найдите сумму чисел  n + nn + nnn")))
+
+n = (5)
+
+user_message = 615
+
+print(615)
+
+
+a = str (16000)
+b = str( 10000)
+c = int   ( a - b)
+print (a -  b)
+
+a = str("выручка")
+b = str("издержки")
+c = int("прибыль")
+print(type)
+c =  int(input ("С каким результатом работает фирма?"))
+
+if c > b :
+    print("Прибыль больше издержек")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
